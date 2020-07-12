46a3aba0a08359f257190d1a2da510e48f880c9c
wangtao <wangtaohandsome@gamil.com>
2020-07-12 19:02:28 +0800

    'todo需要完善插件换肤'

--- app/src/main/java/com/example/customizeui/MainActivity.kt
+++ app/src/main/java/com/example/customizeui/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.customizeui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.everday.skinplugin.SkinManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //换肤插件
+        SkinManager.getInstance().loadSkin("")
     }
 }
